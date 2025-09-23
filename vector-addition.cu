8c0991aee20af5d4e0b7c5406e7468ac2afe38d6
MeriGasparyan <merigas04@gmail.com>
2025-09-23 21:05:55 +0400

    add vector addition

---
+++ vector-addition.cu
@@ -0,0 +1,33 @@
+#include <cuda_runtime.h>
+#include <cmath> // for ceil if needed
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, int n) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < n) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+void vecAdd(float* A, float* B, float* C, int n) {
+    float *A_d, *B_d, *C_d;
+    size_t size = n * sizeof(float);
+
+    cudaMalloc((void**)&A_d, size);
+    cudaMalloc((void**)&B_d, size);
+    cudaMalloc((void**)&C_d, size);
+
+    cudaMemcpy(A_d, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B, size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+
+    vecAddKernel<<<blocksPerGrid, threadsPerBlock>>>(A_d, B_d, C_d, n);
+
+    cudaMemcpy(C, C_d, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+}
